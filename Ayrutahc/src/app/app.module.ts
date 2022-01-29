99b5b63a28d79c281a91e10c2e7522a9c23e1682
srirams diddi <srirams.diddi@hotmail.com>
2022-01-30 01:24:59 +0530

    commit changes in home page section

--- Ayrutahc/src/app/app.module.ts
+++ Ayrutahc/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule.forRoot(routes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
